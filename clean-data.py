dd8991bd1115a7e26364254649cbb06b607f81d8
Jeff Everhart <Jeffeverhart383@gmail.com>
2017-06-19 14:52:18 -0400

    update to add CDC data to topojson file

--- clean-data.py
+++ clean-data.py
@@ -2,6 +2,17 @@ import csv, json
 
 # create a dictionary to hold all of the state data
 states_dict = {}
+id_list = []
+
+
+# read in the topojson atlas file 
+with open('us-atlas.json') as atlasfile: 
+	atlas_reader = json.load(atlasfile)
+	print(atlas_reader)
+	for state in atlas_reader['objects']['states']['geometries']: 
+		id_list.append(state['id'])
+
+print(len(id_list))
 
 # read the file of state names/abbreviations
 with open('states.csv') as statesfile:
@@ -9,7 +20,7 @@ with open('states.csv') as statesfile:
 	for line in state_reader: 
 		# create a new key/value pair for each state abbreviation
 		# do this so that you can use the abbrv from the CDC data sets 
-		states_dict[line['Abbreviation']] = line		
+		states_dict[line['Abbreviation']] = line	
 
 # read in 2015 data
 with open('2015-death-data.csv', 'rb') as datafile: 
@@ -30,7 +41,13 @@ with open('2013-2014-death-data.csv', 'rb') as datafile:
 		state['deaths2014'] = line['2014Number']		
 
 with open('combined-data.json', 'w') as outfile: 
-	json.dump(states_dict, outfile)
+	with open('us-atlas.json') as atlasfile: 
+		atlas_reader = json.load(atlasfile)
+		for state in atlas_reader['objects']['states']['geometries']:
+			for key, value in states_dict.iteritems():
+				if(value['AtlasId'] == state['id']):
+					state['properties'] = value
+	json.dump(atlas_reader, outfile)
 		
 
 
